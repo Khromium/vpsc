7fc70b50217f658344eed7e3689eb6f419dde925
Kei <devs@outlook.jp>
2023-12-26 11:10:35 +0900

    add endpoints (#5)
    * add endpoints
    
    * add server tests
    
    * add nfs and switch tests
    
    * fix import problems

--- tests/test_servers.py
+++ tests/test_servers.py
@@ -1,30 +1,148 @@
 import unittest
 
+from vpsc.models import UpdateServer, ShutdownServer, UpdateHost
 from vpsc.client import Client, APIConfig
-from .request_loader import request_loader
+from .patch_request import patch_request
 
 
 class TestServers(unittest.TestCase):
     def setUp(self):
         self.client = Client(config=APIConfig(api_key="test"))
 
-    @request_loader("server_200")
-    def test_server(self, patched):
+    @patch_request("server_200")
+    def test_get_server(self, patched):
         server = self.client.get_server(server_id=1)
-        assert server.id == 0
+        assert 0 == server.id
         patched.assert_called_once_with(
             method="get",
             url=f"{self.client.config.host}/servers/1",
             headers={"Authorization": f"Bearer {self.client.config.api_key}"},
         )
 
-    @request_loader("servers_200")
-    def test_servers(self, patched):
+    @patch_request("servers_200")
+    def test_get_servers(self, patched):
         servers = self.client.get_servers()
-        assert len(servers) == 1
-        assert list(servers)[0].id == 0
+        assert 1 == len(servers)
+        assert 0 == list(servers)[0].id
         patched.assert_called_once_with(
             method="get",
             url=f"{self.client.config.host}/servers",
             headers={"Authorization": f"Bearer {self.client.config.api_key}"},
+        )
+
+    @patch_request("server_200")
+    def test_update_servers(self, patched):
+        data = UpdateServer(name="name_test", description="description_test")
+        result = self.client.update_server(server_id=0, data=data)
+        assert 0 == result.id
+        patched.assert_called_once_with(
+            method="put",
+            url=f"{self.client.config.host}/servers/0",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+            data=data.model_dump_json(exclude_unset=True),
+        )
+
+    @patch_request("server_power_status_200")
+    def test_get_power_status(self, patched):
+        result = self.client.get_server_power_status(server_id=0)
+        assert "power_on" == result.status
+        patched.assert_called_once_with(
+            method="get",
+            url=f"{self.client.config.host}/servers/0/power-status",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}"},
+        )
+
+    @patch_request("status_202")
+    def test_power_on_server(self, patched):
+        result = self.client.power_on_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/power-on",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+        )
+
+    @patch_request("status_202")
+    def test_shutdown_server(self, patched):
+        result = self.client.power_on_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/power-on",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+        )
+
+    @patch_request("status_202")
+    def test_force_reboot_server(self, patched):
+        result = self.client.power_on_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/power-on",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+        )
+
+    @patch_request("status_202")
+    def test_power_on_server(self, patched):
+        result = self.client.power_on_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/power-on",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+        )
+
+    @patch_request("status_202")
+    def test_shutdown_server(self, patched):
+        result = self.client.shutdown_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/shutdown",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+            data=ShutdownServer(force=False).model_dump_json(exclude_unset=True),
+        )
+
+        patched.reset_mock()
+        result = self.client.shutdown_server(server_id=0, force=True)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/shutdown",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+            data=ShutdownServer(force=True).model_dump_json(exclude_unset=True),
+        )
+
+    @patch_request("status_202")
+    def test_force_reboot_server(self, patched):
+        result = self.client.force_force_reboot_server(server_id=0)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/force-reboot",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+        )
+
+    @patch_request("status_200")
+    def test_server_ipv4_ptr(self, patched):
+        data = UpdateHost(hostname="example.com")
+        result = self.client.update_server_ipv4_ptr(server_id=0, data=data)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/ipv4-ptr",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+            data=data.model_dump_json(exclude_unset=True),
+        )
+
+    @patch_request("status_200")
+    def test_server_ipv6_ptr(self, patched):
+        data = UpdateHost(hostname="example.com")
+        result = self.client.update_server_ipv6_ptr(server_id=0, data=data)
+        assert result is None
+        patched.assert_called_once_with(
+            method="post",
+            url=f"{self.client.config.host}/servers/0/ipv6-ptr",
+            headers={"Authorization": f"Bearer {self.client.config.api_key}", "content-type": "application/json"},
+            data=data.model_dump_json(exclude_unset=True),
         )
--- vpsc/client.py
+++ vpsc/client.py
@@ -1,10 +1,23 @@
 from types import MappingProxyType
-from typing import Optional, Iterable
+from typing import Optional, Iterable, List
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .api_request import APIRequest
-from .models import server_sort_query, Server
+from .models import (
+    server_sort_query,
+    Server,
+    UpdateServer,
+    ServerPowerStatus,
+    UpdateHost,
+    NfsServer,
+    NfsServerPowerStatus,
+    CreateSwitch,
+    Switch,
+    UpdateSwitch,
+    UpdateNfsServerIpv4,
+    ShutdownServer,
+)
 
 
 class APIConfig(BaseSettings):
@@ -17,6 +30,7 @@ class Client:
     def __init__(self, config: APIConfig):
         self.config = config
         self.header = MappingProxyType({"Authorization": f"Bearer {self.config.api_key}"})
+        self.client = APIRequest(config=self.config, header=self.header)
 
     def get_servers(self, sort: Optional[server_sort_query] = None) -> Iterable[Server]:
         """
@@ -24,7 +38,7 @@ class Client:
         :param sort: ソート情報
         :return:
         """
-        return APIRequest(config=self.config, header=self.header).request(
+        return self.client.request(
             endpoint="/servers",
             method="get",
             response_obj=Server,
@@ -33,11 +47,206 @@ class Client:
     def get_server(self, server_id: int) -> Server:
         """
         個別のサーバー情報を取得する
-        :param server_id: サーバーid
+        :param server_id: サーバーID
         :return:
         """
-        return APIRequest(config=self.config, header=self.header).request(
+        return self.client.request(
             endpoint=f"/servers/{server_id}",
             method="get",
             response_obj=Server,
+        )
+
+    def update_server(self, server_id: int, data: UpdateServer) -> Server:
+        """
+        個別のサーバー情報を更新する
+        :param server_id: サーバーID
+        :param data: 更新データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}",
+            method="put",
+            data=data,
+            response_obj=Server,
+        )
+
+    def get_server_power_status(self, server_id: int) -> ServerPowerStatus:
+        """
+        サーバーの電源状態を取得する
+        :param server_id:　サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/power-status",
+            method="get",
+            response_obj=ServerPowerStatus,
+        )
+
+    def power_on_server(self, server_id: int):
+        """
+        サーバーを起動する
+        :param server_id: サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/power-on",
+            method="post",
+        )
+
+    def shutdown_server(self, server_id: int, force: bool = False):
+        """
+        サーバーをシャットダウンする
+        :param force: 強制停止を行うか
+        :param server_id: サーバーID
+        :return:
+        """
+        data = ShutdownServer(force=force)
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/shutdown",
+            method="post",
+            data=data,
+        )
+
+    def force_force_reboot_server(self, server_id: int):
+        """
+        サーバーを強制再起動する
+        :param server_id: サーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/force-reboot",
+            method="post",
+        )
+
+    def update_server_ipv4_ptr(self, server_id: int, data: UpdateHost):
+        """
+        サーバーのipv4の逆引きホスト名を設定する
+        :param server_id: サーバーID
+        :param data: 設定データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/ipv4-ptr",
+            method="post",
+            data=data,
+        )
+
+    def update_server_ipv6_ptr(self, server_id: int, data: UpdateHost):
+        """
+        サーバーのipv6の逆引きホスト名を設定する
+        :param server_id: サーバーID
+        :param data: 設定データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/servers/{server_id}/ipv6-ptr",
+            method="post",
+            data=data,
+        )
+
+    def get_nfs_servers(self) -> List[NfsServer]:
+        """
+        NFSサーバー情報一覧を取得する
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers",
+            method="get",
+            response_obj=NfsServer,
+        )
+
+    def get_nfs_server(self, nfs_server_id: int) -> NfsServer:
+        """
+        個別のNFSサーバー情報を取得する
+        :param nfs_server_id: NFSサーバーID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}",
+            method="get",
+            response_obj=NfsServer,
+        )
+
+    def update_nfs_server_ipv4(self, nfs_server_id: int, data: UpdateNfsServerIpv4):
+        """
+        NFSサーバーのipv4を設定する
+        :param nfs_server_id: NFSサーバーID
+        :param data: 設定情報
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}/ipv4",
+            method="post",
+            data=data,
+        )
+
+    def get_nfs_server_power_status(self, nfs_server_id: int) -> NfsServerPowerStatus:
+        """
+        NFSサーバーの電源状態を取得する
+        :param nfs_server_id:
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/nfs-servers/{nfs_server_id}/power-status", method="get", response_obj=NfsServerPowerStatus
+        )
+
+    def create_switch(self, data: CreateSwitch) -> Switch:
+        """
+        スイッチを作成する
+        :param data: 作成データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches",
+            method="post",
+            data=data,
+            response_obj=Switch,
+        )
+
+    def get_switches(self) -> List[Switch]:
+        """
+        スイッチ情報一覧を取得する
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches",
+            method="get",
+            response_obj=Switch,
+        )
+
+    def get_switch(self, switch_id: int) -> Switch:
+        """
+        個別のスイッチ情報を取得する
+        :param switch_id: スイッチID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="get",
+            response_obj=Switch,
+        )
+
+    def update_switch(self, switch_id: int, data: UpdateSwitch) -> Switch:
+        """
+        個別のスイッチ情報を更新する
+        :param switch_id: スイッチID
+        :param data: 更新データ
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="put",
+            data=data,
+            response_obj=Switch,
+        )
+
+    def delete_switch(self, switch_id: int):
+        """
+        個別のスイッチを削除する
+        :param switch_id: スイッチID
+        :return:
+        """
+        return self.client.request(
+            endpoint=f"/switches/{switch_id}",
+            method="delete",
         )
